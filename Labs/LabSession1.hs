3b2d3f076f4cb77f2baa29f022d39da09c92f9d9
Mark <marktmmrmn@gmail.com>
2016-09-22 15:25:30 +0200

    Exercises 2, 4 and a part of 7 added.

--- Labs/LabSession1.hs
+++ Labs/LabSession1.hs
@@ -19,10 +19,42 @@ getSumSquareDigits x
 
 -- Exercise 2: Takuzu Bitstrings 
 takuzuStrings :: Int -> [[Char]]
-takuzuStrings n
+takuzuStrings n = recursiveTakuzu n ""
 
-checkTakuzu :: [Char] -> Bool
-checkTakuzu string
 
-  | n == 0 = True
-  | otherwise = '1' ++ checkTakuzu(n-1)
+recursiveTakuzu :: Int -> [Char] -> [[Char]]
+recursiveTakuzu n string
+  | n == 0 = [reverse string]
+  | (take 2 string) == "11" = recursiveTakuzu (n-1) ('0':string)
+  | (take 2 string) == "00" = recursiveTakuzu (n-1) ('1':string)
+  | otherwise = recursiveTakuzu (n-1) ('0':string) ++ recursiveTakuzu (n-1) ('1':string)
+
+
+-- Exercise 4: Last n digits
+lastDigits :: Int -> Int -> [Int]
+lastDigits n d =
+  drop (length digits - d) digits
+  where digits = getDigits (sum [n^p | p <- [0 .. n]])
+
+getDigits :: Int -> [Int]
+getDigits x
+  | x == 0 = []
+  | otherwise = getDigits(x `div` 10) ++ [x `mod` 10]
+
+-- Exercise 7: Takuzu solver
+-- -- isCorrectTakuzu :: [[Char]] -> Bool
+-- -- isCorrectTakuzu =
+-- recursiveTakuzuSolver :: [[Char]] -> Bool
+-- recursiveTakuzuSolver grid
+--   | n == 0 && checkTakuzuString 8 string = True
+--   with corrects takuzuStrings (length grid !! 1)
+
+
+takuzuStringsCorrect :: Int -> [[Char]]
+takuzuStringsCorrect n = [x | x <- recursiveTakuzu n "", length (filter (=='1') x) == (n `div` 2)]
+
+checkTakuzuString :: Int -> [Char] -> Bool
+checkTakuzuString n string = elem string (takuzuStringsCorrect n)
+
+dotLocations :: [[Char]] -> [(Int, Int)]
+dotLocations grid = [(x,y) | x <- [0..8], y <- [0..8], ((grid !! y) !! x) == '.']
